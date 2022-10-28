67c2a94e74ea5d3399895cd4145dc539affeeb6d
arkhon7 <reviuy9@gmail.com>
2022-10-28 16:12:39 +0800

    added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mizuki
+
+Mizuki is a _tiny_ javascript library that helps you create your own custom scrollers, carousels, or anything similar. It only focuses on handling `index` state so you only have to focus on styling your document.
+
+### yarn
+
+```
+yarn add mizuki
+```
+
+### npm
+
+```
+npm install mizuki
+```
+
+# How to Use
---
+++ tests/vanilla-app/main.js
@@ -0,0 +1,35 @@
+import './style.css'
+import mizuki from "mizuki"
+
+// initialize mizuki to create throttle and index context
+const mzk = mizuki()
+
+// initialize controls and config to create getters and setters
+const  [get, set] = mzk({delay: 500})
+
+
+const incrementButton =document.querySelector(".inc")
+const decrementButton = document.querySelector(".dec")
+const counter = document.querySelector(".counter")
+
+
+counter.innerHTML = get() // get the initial index
+
+
+incrementButton.addEventListener("click", () => {
+
+  set((index) => index + 1) // set the index
+  counter.innerHTML = get() // update the index
+
+})
+
+
+decrementButton.addEventListener("click", () => {
+
+  set((index) => index - 1)
+  counter.innerHTML = get()
+
+})
+
+
+
