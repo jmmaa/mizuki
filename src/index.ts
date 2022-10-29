8e3fe8b05a9fa2128be24a8a0665df9d65dddc0f
arkhon7 <reviuy9@gmail.com>
2022-10-29 22:43:47 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,114 +1,81 @@
 type MizukiOptions = {
   init?: number;
   delay?: number;
-  min?: number;
-  max?: number;
+  bounds?: {
+    min: number;
+    max: number;
+  };
   loop?: boolean;
 };
 
 type DefaultMizukiOptions = {
   init: number;
   delay: number;
-  min: number | undefined;
-  max: number | undefined;
+  bounds?: {
+    min: number;
+    max: number;
+  };
   loop: boolean;
 };
 
-const wraps = <T, R>(value: T | R) => {
-  return {
-    value: value,
-    bind: <N>(callback: (value: N) => N) => wraps(callback(<N>value)),
-  };
+const defaultOptions: DefaultMizukiOptions = {
+  init: 0,
+  delay: 0,
+  bounds: undefined,
+  loop: false,
 };
 
-const ensure = <T, R>(value: T, fallback: R) =>
-  wraps<T, R>(value).bind<R>((v) => (v ? <R>v : <R>fallback));
+export const mizuki = (options?: MizukiOptions) => {
+  const __options = options
+    ? { ...defaultOptions, ...options }
+    : defaultOptions;
 
-const ensureOptions = (options?: MizukiOptions): DefaultMizukiOptions => {
-  const __defaultOpts: DefaultMizukiOptions = {
-    init: 0,
-    delay: 0,
-    min: undefined,
-    max: undefined,
-    loop: false,
-  };
+  const { init, delay, bounds, loop } = __options;
 
-  const __options = ensure(options, __defaultOpts).value;
-
-  if (
-    !(
-      (__options.min === undefined && __options.max === undefined) ||
-      (typeof __options.min === "number" && typeof __options.max === "number")
-    )
-  ) {
-    throw new Error(
-      "you have to set both 'min' and 'max' and not just one of them!"
-    );
-  }
-
-  return {
-    ...__defaultOpts,
-    ...__options,
-  };
-};
-
-const mizuki = (options?: MizukiOptions) => {
-  const globalOptions = ensureOptions(options);
-
-  let index: number = globalOptions.init;
-
+  let index: number = init;
   let timeout: any | null = null;
 
-  return (
-    options?: MizukiOptions
-  ): [get: () => number, set: (cb: (index: number) => number) => void] => {
-    const { init, delay, min, max, loop } = options
-      ? { ...globalOptions, ...options }
-      : globalOptions;
-
-    index = init;
-
-    const get = () => {
-      return index;
-    };
-
-    const set = (cb: (index: number) => number) => {
-      const timeoutStart = () => {
-        timeout = setTimeout(() => {
-          timeout = null;
-        }, delay);
-      };
-
+  const startTimeout = () => {
+    // check for true non-delay
+    if (delay > 0) {
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, delay);
+    }
+  };
+  return {
+    get: () => index,
+    set: (cb: (index: number) => number) => {
       let newIndex = cb(index);
 
       if (timeout === null) {
-        if (min !== undefined && max !== undefined) {
+        if (bounds !== undefined) {
+          const { min, max } = bounds;
+
           if (min <= newIndex && max >= newIndex) {
             index = newIndex;
-            timeoutStart();
+            startTimeout();
           }
 
           if (newIndex < min) {
             if (loop) {
               index = max;
-              timeoutStart();
+              startTimeout();
             }
           }
 
           if (newIndex > max) {
             if (loop) {
               index = min;
-              timeoutStart();
+              startTimeout();
             }
           }
         } else {
           index = newIndex;
-          timeoutStart();
+          startTimeout();
         }
       }
-    };
-
-    return [get, set];
+    },
   };
 };
 
