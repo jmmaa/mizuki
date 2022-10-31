7b5f89686b9f3cc7445cba73328b2782d5d1694f
arkhon7 <reviuy9@gmail.com>
2022-10-31 16:51:30 +0800

    updated tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,14 +2,9 @@ import mizuki from "../src/index";
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-test("for config", () => {
-  mizuki();
-  mizuki({});
-  mizuki({ delay: 1000, bounds: { min: 0, max: 3 }, loop: true });
-});
-
 test("get/set functions", () => {
-  const { get, set } = mizuki();
+  const engine = mizuki();
+  const { get, set } = engine();
 
   expect(get()).toEqual(0);
   set((index) => index + 1);
@@ -18,10 +13,14 @@ test("get/set functions", () => {
 
   set((index) => index - 1);
   expect(get()).toEqual(0);
+
+  set(69);
+  expect(get()).toEqual(69);
 });
 
 test("for using delay", async () => {
-  const { get, set } = mizuki({ delay: 1000 });
+  const engine = mizuki();
+  const { get, set } = engine({ delay: 1000 });
 
   expect(get()).toEqual(0);
   set((index) => index + 1);
@@ -37,7 +36,8 @@ test("for using delay", async () => {
 });
 
 test("for using bounds", () => {
-  const { get, set } = mizuki({ bounds: { min: 0, max: 3 } });
+  const engine = mizuki();
+  const { get, set } = engine({ bounds: { min: 0, max: 3 } });
 
   expect(get()).toEqual(0);
 
@@ -63,7 +63,8 @@ test("for using bounds", () => {
 });
 
 test("for using loop", () => {
-  const { get, set } = mizuki({ bounds: { min: 0, max: 3 }, loop: true });
+  const engine = mizuki();
+  const { get, set } = engine({ bounds: { min: 0, max: 3 }, loop: true });
 
   expect(get()).toEqual(0);
 
@@ -80,4 +81,20 @@ test("for using loop", () => {
   set((index) => index - 1);
 
   expect(get()).toEqual(3);
+});
+
+test("benchmark", () => {
+  const engine = mizuki();
+
+  let start = performance.now();
+  const { set } = engine({ bounds: { min: 0, max: 3 }, loop: true });
+  const count = 100_000_000;
+
+  for (let i = 0; i < count; i++) {
+    set((index) => index + 1);
+  }
+
+  let end = performance.now();
+
+  console.log(`time it took for ${count} set calls: ${end - start}ms`);
 });
