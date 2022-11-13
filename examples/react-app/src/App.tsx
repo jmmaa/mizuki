e31d7d577868adbee8ccfccfec44d644a2190dbd
arkhon7 <reviuy9@gmail.com>
2022-11-13 17:38:43 +0800

    updated examples

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -4,48 +4,61 @@ import React from "react";
 
 const ease = bezier(0.25, 0.1, 0.25, 1.0);
 
+// custom hooks for mizuki
+const useAnimate = () => React.useRef(mizuki.createAnimation()).current;
+const useAction = (options: any) => React.useRef(mizuki.createAction(options)).current;
+
 export default function MyFullpage() {
   // FIX THIS
 
-  let index = 0;
-  let lastVal = 0;
-  let lastTransformVal = 0;
-
-  const set = React.useMemo(() => mizuki.action({ delay: 500 }), []);
+  const indexRef = React.useRef(0);
+  const setter = useAction({ delay: 1000, min: 0, max: 3 });
+  const animate = useAnimate();
   const scrollerRef = React.useRef<HTMLDivElement>(null);
 
-  const wheelHandler = (event: React.WheelEvent) => {
+  const wheelHandler = (event: any) => {
     if (event.deltaY > 0) {
-      const newIndex = set(index + 1);
+      const newIndex = setter(indexRef.current + 1);
 
       if (newIndex === undefined) return;
-      index = newIndex;
+      console.log(newIndex);
+      indexRef.current = newIndex;
 
-      mizuki.createAnimationFrames((delta) => {
-        let transformValue = lastVal - ease(delta) * 100;
+      animate((delta, previousTransitionValue) => {
         if (scrollerRef.current === null) return;
-        scrollerRef.current.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
-        lastTransformVal = transformValue;
+        const transitionValue = previousTransitionValue - ease(delta) * 100;
+        scrollerRef.current.style.transform = `translate3d(0, ${transitionValue}vh, 0)`;
+
+        return () => transitionValue;
       }, 1000);
     } else if (event.deltaY < 0) {
-      const newIndex = set(index - 1);
+      const newIndex = setter(indexRef.current - 1);
 
       if (newIndex === undefined) return;
-      index = newIndex;
+      console.log(newIndex);
+      indexRef.current = newIndex;
 
-      mizuki.createAnimationFrames((delta) => {
-        let transformValue = lastVal + ease(delta) * 100;
+      animate((delta, previousTransitionValue) => {
         if (scrollerRef.current === null) return;
-        scrollerRef.current.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
-        lastTransformVal = transformValue;
+        const transitionValue = previousTransitionValue + ease(delta) * 100;
+        scrollerRef.current.style.transform = `translate3d(0, ${transitionValue}vh, 0)`;
+
+        return () => transitionValue;
       }, 1000);
     }
-    lastVal = lastTransformVal;
   };
+
+  React.useEffect(() => {
+    if (scrollerRef.current === null) return;
+
+    scrollerRef.current.addEventListener("wheel", wheelHandler);
+
+    return () => scrollerRef.current?.removeEventListener("wheel", wheelHandler);
+  });
 
   return (
     <div style={{ width: "100vw", height: "100vh", overflowY: "hidden" }}>
-      <div ref={scrollerRef} onWheel={wheelHandler}>
+      <div className="scroller" ref={scrollerRef}>
         <div style={{ width: "100vw", height: "100vh", background: "#ff5f45" }}>Kanade</div>
         <div style={{ width: "100vw", height: "100vh", background: "#0798ec" }}>Mafuyu</div>
         <div style={{ width: "100vw", height: "100vh", background: "#fc6c7c" }}>Ena</div>
--- examples/vanilla-app/main.js
+++ examples/vanilla-app/main.js
@@ -1,26 +1,19 @@
 import './style.css'
 import mizuki from "mizuki"
-
 import bezier from 'bezier-easing'
 
 
 
-const ease = bezier(0.25, 0.1, 0.25, 1.0)
-
-const shift = mizuki.action({ min: 0, max: 3, delay: 1000, loop: false})
-let index = 0
-let lastVal = 0
-let lastTransformVal = 0
-let remaining = 100
-
-
-const scrollerContent = document.querySelector(".scroller-content")
 const scroller = document.querySelector(".scroller")
+const scrollerContent = document.querySelector(".scroller-content")
 
+const ease = bezier(0.25, 0.1, 0.25, 1.0)
+const shift = mizuki.createAction({ min: 0, max: 3, delay: 1000, loop: false})
+const animate = mizuki.createAnimation()
+
+let index = 0
 
 scroller.addEventListener("wheel", (e) => {
-
-
 
   if (e.deltaY > 0) {
     const newIndex = shift(index + 1)
@@ -28,26 +21,32 @@ scroller.addEventListener("wheel", (e) => {
     if (newIndex === undefined) return
     index = newIndex
 
-    mizuki.createAnimationFrames((delta) => {
-      let transformValue = lastVal - ease(delta) * 100
-      scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`
-      lastTransformVal = transformValue
+    animate((delta, previousTransitionValue) => {
+
+      const transitionValue = previousTransitionValue - ease(delta) * 100 
+      scrollerContent.style.transform = `translate3d(0, ${transitionValue}vh, 0)`
+
+      return () => transitionValue
+
     }, 1000)
+    
 
   } else if (e.deltaY < 0) {
+
     const newIndex = shift(index - 1)
-    
+
     if (newIndex === undefined) return
     index = newIndex
 
-    mizuki.createAnimationFrames((delta) => {
-      let transformValue = lastVal + ease(delta) * 100 
-      scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`
-      lastTransformVal = transformValue
+
+    animate((delta, previousTransitionValue) => {
+
+      const transitionValue = previousTransitionValue + ease(delta) * 100 
+      scrollerContent.style.transform = `translate3d(0, ${transitionValue}vh, 0)`
+      
+      return () => transitionValue
+
     }, 1000)
-    
   }
-  lastVal = lastTransformVal
-  
 })
 
