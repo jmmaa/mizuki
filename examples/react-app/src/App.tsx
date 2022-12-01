d122e582e3a867587ef10f5fd4c329626732414b
arkhon7 <reviuy9@gmail.com>
2022-12-01 20:18:18 +0800

    updated examples

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -6,37 +6,39 @@ const ease = bezier(0.25, 0.1, 0.25, 1.0);
 
 // custom hooks for mizuki v2
 
-const useTracker = (options: any) => React.useRef(mizuki.createIndexTracker(options)).current;
+const useTracker = (options: any) => React.useRef(mizuki.createTracker(options)).current;
 const useTimeout = () => React.useRef(mizuki.createTimeout()).current;
-const useAnimate = () => React.useRef(mizuki.createAnimation()).current;
+const useNonReactiveState = <T,>(initState: T) =>
+  React.useRef(mizuki.createState(initState)).current;
 
 function MyFullpage() {
-  const [transformValue, setTransformValue] = React.useState(0);
-
-  const animate = useAnimate();
-
   const [allowedToGo] = useTracker({ delay: 1000, min: 0, max: 3 });
   const [timeout, isTimedout] = useTimeout();
+  const [getLastUnits, setLastUnits] = useNonReactiveState(0);
 
-  const createWheelHandler = (el: HTMLElement | null) => {
+  const createWheelHandler = (el: HTMLElement) => {
     return (event: any) => {
       if (isTimedout()) return;
 
       if (event.deltaY > 0) {
         if (!allowedToGo((index) => index + 1)) return;
 
-        if (el === null) return;
-        animate({ units: -100, duration: 1000 }, (units) => {
-          el.style.transform = `translate3d(0, ${units}vh, 0)`;
+        mizuki.createAnimation({ duration: 1000 }, (delta) => {
+          const transformValue = getLastUnits() + ease(delta) * -100;
+          el.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
+
+          return () => setLastUnits(() => transformValue);
         });
 
         timeout(1000);
       } else if (event.deltaY < 0) {
         if (!allowedToGo((index) => index - 1)) return;
 
-        if (el === null) return;
-        animate({ units: 100, duration: 1000 }, (units) => {
-          el.style.transform = `translate3d(0, ${units}vh, 0)`;
+        mizuki.createAnimation({ duration: 1000 }, (delta) => {
+          const transformValue = getLastUnits() + ease(delta) * 100;
+          el.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
+
+          return () => setLastUnits(() => transformValue);
         });
 
         timeout(1000);
--- examples/vanilla-app/main.js
+++ examples/vanilla-app/main.js
@@ -3,55 +3,50 @@ import mizuki from "mizuki"
 import bezier from 'bezier-easing'
 
 
-const scroller = document.querySelector(".scroller")
+
+const ease = bezier(0.25, 0.1, 0.25, 1.0);
+const scroller = document.querySelector(".scroller");
 const scrollerContent = document.querySelector(".scroller-content")
-const ease = bezier(0.25, 0.1, 0.25, 1.0)
 
-
-
-const animate = mizuki.createAnimation()
-const onWheel =  mizuki.createEventObserver(scroller, "wheel")
-const [timeout, isTimedout] = mizuki.createTimeout()
-const [allowedToGo] = mizuki.createIndexTracker({max: 3, min: 0, loop: false})
+const [allowedToGo] = mizuki.createTracker({ max: 3, min: 0, loop: false });
+const [timeout, isTimedout] = mizuki.createTimeout();
+const [getLastUnits, setLastUnits] = mizuki.createState(0);
 
 const wheelHandler = (event) => {
-    if (isTimedout()) return; // throttle if timeout still persists
+  if (isTimedout()) return; // throttle if timeout still persists
 
-    if (event.deltaY > 0) {
+  if (event.deltaY < 0) { // go 
+    
+    if (allowedToGo((index) => index - 1)) { // check if allowed to go to the next slide
 
-      if (allowedToGo((index) => index + 1)) { // check if allowed to go to the next slide
+      mizuki.createAnimation({ duration: 1000 }, (delta) => {
 
-        animate({ units: -100, duration: 1000 }, (units) => {
+        const transformValue = getLastUnits() + ease(delta) * 100;
+        scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
 
-          // apply style changes here every frame
-          scrollerContent.style.transform = `translate3d(0, ${units}vh, 0)`;
-        }, 1000)
+        return () => setLastUnits(() => transformValue); // this will execute after requestAnimationFrame loop
+      });
 
-        timeout(1000); // timeout for 1 seconds after changing slide
-      }
 
-      
-    } else if (event.deltaY < 0) {
-
-      if (allowedToGo((index) => index - 1)) {
-
-        animate({ units: 100, duration: 1000 }, (units) => {
-
-          scrollerContent.style.transform = `translate3d(0, ${units}vh, 0)`;
-
-        }, 1000)
-
-        timeout(1000);
-
-      }
+      timeout(1000); // timeout for 1 second after changing slide
     }
-  };
+  } else if (event.deltaY > 0) {
 
+    if (allowedToGo((index) => index + 1)) {
 
+      mizuki.createAnimation({ duration: 1000 }, (delta) => {
 
-onWheel(wheelHandler)
+        const transformValue = getLastUnits() + ease(delta) * -100;
+        scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
 
-// scroller.addEventListener("wheel", wheelHandler)
+        return () => setLastUnits(() => transformValue);
+      });
 
-// TRY WRAPPING THE EVENT IN createState
+      timeout(1000);
+    }
+  }
+};
+
+scroller.addEventListener("wheel", wheelHandler);
+
 
