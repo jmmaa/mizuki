b21225731e3dae7d57d59aa45a334cb8002884cb
arkhon7 <reviuy9@gmail.com>
2022-12-02 14:01:16 +0800

    added benchmark tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -24,29 +24,25 @@ test("createState", () => {
 
   expect(get()).toEqual(0);
 
-  set((index) => {
-    return index + 1;
-  });
+  set((index) => index + 1);
 
   expect(get()).toEqual(1);
 
-  set((index) => {
-    return index + 69;
-  });
+  set((index) => index + 69);
 
-  set((index) => {
-    return index + 100;
-  });
+  set((index) => index + 100);
 
-  set((index) => {
-    return index + 100;
-  });
+  set((index) => index + 100);
 
-  set(() => {
-    return 7;
-  });
+  set(() => 7);
 
   expect(get()).toEqual(7);
+
+  for (let i = 0; i < 50; i++) {
+    set((index) => index + 1);
+  }
+
+  expect(get()).toEqual(57);
 });
 
 test("createObserver", () => {
@@ -62,6 +58,87 @@ test("createObserver", () => {
   notify(curr);
 
   curr -= 1;
-
   notify(curr);
+});
+
+test("benchmark calculate", () => {
+  const count = 100_000_000;
+
+  const benchmark = (iterations: number) => {
+    let total = 0;
+
+    for (let j = 0; j < iterations; j++) {
+      const start = performance.now();
+
+      let index = 0;
+      for (let i = 0; i < count; i++) {
+        index = mizuki.calculate(index, 3, 0, true);
+      }
+
+      const end = performance.now();
+
+      total += end - start;
+    }
+
+    return total / iterations;
+  };
+
+  console.log(`time it took to perform 1m calc calls: ${Math.round(benchmark(1000))}ms`);
+});
+
+test("benchmark createState", () => {
+  const [get, set] = mizuki.createState(0);
+  const count = 100_000_000;
+
+  const benchmark = (iterations: number) => {
+    let total = 0;
+    let index = 0;
+
+    for (let j = 0; j < iterations; j++) {
+      const start = performance.now();
+
+      for (let i = 0; i < count; i++) {
+        set((i) => i + 1);
+        index = get();
+      }
+
+      const end = performance.now();
+
+      total += end - start;
+    }
+
+    return total / iterations;
+  };
+
+  console.log(`time it took to perform 1m calc calls: ${Math.round(benchmark(1))}ms`);
+});
+
+test("benchmark createController", () => {
+  const [get, set, valid] = mizuki.createController({ min: 0, max: 3, loop: true });
+  const count = 100_000_000;
+
+  const benchmark = (iterations: number) => {
+    let total = 0;
+    let index = 0;
+
+    for (let j = 0; j < iterations; j++) {
+      const start = performance.now();
+
+      for (let i = 0; i < count; i++) {
+        if (valid((i) => i + 1)) {
+          set((i) => i + 1);
+          index = get();
+        }
+      }
+
+      const end = performance.now();
+
+      total += end - start;
+    }
+    console.log(index);
+
+    return total / iterations;
+  };
+
+  console.log(`time it took to perform 1m calc calls: ${Math.round(benchmark(1))}ms`);
 });
