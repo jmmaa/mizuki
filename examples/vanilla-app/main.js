e47f046a5c4d5cbd101e8fc7639dc14e2ea48f22
arkhon7 <reviuy9@gmail.com>
2022-12-02 13:59:50 +0800

    updated examples

--- examples/vanilla-app/main.js
+++ examples/vanilla-app/main.js
@@ -3,46 +3,61 @@ import mizuki from "mizuki"
 import bezier from 'bezier-easing'
 
 
-
 const ease = bezier(0.25, 0.1, 0.25, 1.0);
 const scroller = document.querySelector(".scroller");
 const scrollerContent = document.querySelector(".scroller-content")
 
-const [allowedToGo] = mizuki.createTracker({ max: 3, min: 0, loop: false });
-const [timeout, isTimedout] = mizuki.createTimeout();
+const [, go, canGo] = mizuki.createController({ max: 3, min: 0, loop: false });
+const [timeout, timedout] = mizuki.createTimeout();
 const [getLastUnits, setLastUnits] = mizuki.createState(0);
 
-const wheelHandler = (event) => {
-  if (isTimedout()) return; // throttle if timeout still persists
 
-  if (event.deltaY < 0) { // go 
+
+const next = (index) => index + 1
+const prev = (index) => index - 1
+
+const wheelHandler = (event) => {
+  if (timedout()) return; // debounce if timeout still persists
+
+  if (event.deltaY > 0) {
     
-    if (allowedToGo((index) => index - 1)) { // check if allowed to go to the next slide
+    if (canGo(next)) { // check if allowed to go to the next slide
+
+      go(next) // set the new index
+      timeout(1000); // start timeout for 1 second after setting index
+
+      // animate the element using createAnimation
+      mizuki.createAnimation({ duration: 1000 }, (delta) => { 
+
+        const transformValue = getLastUnits() + ease(delta) * -100;
+        scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
+
+        return () => { // this will execute after the last frame of requestAnimationFrame loop
+
+          setLastUnits(() => transformValue) // save last position for next animation
+
+        };
+      });
+    }
+
+  } else if (event.deltaY < 0) {
+
+    if (canGo(prev)) {
+
+      go(prev);
+      timeout(1000);
 
       mizuki.createAnimation({ duration: 1000 }, (delta) => {
 
         const transformValue = getLastUnits() + ease(delta) * 100;
         scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
 
-        return () => setLastUnits(() => transformValue); // this will execute after requestAnimationFrame loop
+        return () => { 
+
+          setLastUnits(() => transformValue)
+
+        };
       });
-
-
-      timeout(1000); // timeout for 1 second after changing slide
-    }
-  } else if (event.deltaY > 0) {
-
-    if (allowedToGo((index) => index + 1)) {
-
-      mizuki.createAnimation({ duration: 1000 }, (delta) => {
-
-        const transformValue = getLastUnits() + ease(delta) * -100;
-        scrollerContent.style.transform = `translate3d(0, ${transformValue}vh, 0)`;
-
-        return () => setLastUnits(() => transformValue);
-      });
-
-      timeout(1000);
     }
   }
 };
